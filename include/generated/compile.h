8c92004f9a29bdc3add3735cc91b7803a9eca84c
Jason Riordan <jriordan001@gmail.com>
2014-04-05 23:40:36 -0400

    cleanup

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 2 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#2 SMP PREEMPT Tue Feb 26 13:22:02 IST 2013"
-#define LINUX_COMPILE_BY "root"
-#define LINUX_COMPILE_HOST "samarendrab-ubuntu"
-#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
